0fa9ffae63b0d211b37eded7fef6a3b31e26573e
Arjun Araneta <arjunaraneta@gmail.com>
2018-08-31 07:39:35 +0800

    2018 Updates

---
+++ CSharpMSSQLRecordSys/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CSharpMSSQLRecordSys
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source=ALLMANKIND\\MSSQLSERVER8; Database=db_csrecordsys; Integrated Security=True;");
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text == textBox5.Text)
+            {
+                ds = new DataSet();
+                adapter = new SqlDataAdapter("insert into tbl_accounts (username, password, privilege) VALUES " +
+                 "('" + textBox3.Text + "','" + textBox4.Text + "','" + comboBox1.Text + "')", conn);
+                adapter.Fill(ds, "tbl_accounts");
+                MessageBox.Show("User Registered!");
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox5.Clear();
+                comboBox1.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Passwords do not match!");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ds = new DataSet();
+            adapter = new SqlDataAdapter("select * from tbl_accounts where username like '" + textBox1.Text + 
+                "' and password like '" + textBox2.Text + "'", conn);
+            adapter.Fill(ds, "tbl_accounts");
+
+            if (ds.Tables["tbl_accounts"].Rows.Count > 0)
+            {
+                MessageBox.Show("Login Successful!");
+                textBox1.Clear();
+                textBox2.Clear();
+                Form2 frm = new Form2();
+                frm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username and password!");
+            }
+        }
+    }
+}
---
+++ CSharpMSSQLRecordSys/Form3.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CSharpMSSQLRecordSys
+{
+    public partial class Form3 : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source=ALLMANKIND\\MSSQLSERVER8; Database=db_csrecordsys; Integrated Security=True;");
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+        string currentid = "";
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            GetRecords();
+        }
+
+        public void GetRecords()
+        {
+            ds = new DataSet();
+            adapter = new SqlDataAdapter("select * from tbl_accounts", conn);
+            adapter.Fill(ds, "tbl_accounts");
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "tbl_accounts";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ds = new DataSet();
+            adapter = new SqlDataAdapter("insert into tbl_accounts (username, password, privilege) VALUES " +
+             "('" + textBox1.Text + "','" + textBox2.Text + "','" + comboBox1.Text + "')", conn);
+            adapter.Fill(ds, "tbl_accounts");
+            textBox1.Clear();
+            textBox2.Clear();
+            comboBox1.Text = "";
+            GetRecords();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int i = dataGridView1.CurrentRow.Index;
+            currentid = dataGridView1[0, i].Value.ToString();
+
+            ds = new DataSet();
+            adapter = new SqlDataAdapter("delete from tbl_accounts where id = " + currentid, conn);
+            adapter.Fill(ds, "tbl_accounts");
+            GetRecords();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = dataGridView1.CurrentRow.Index;
+            currentid = dataGridView1[0, i].Value.ToString();
+            textBox1.Text = dataGridView1[1, i].Value.ToString();
+            textBox2.Text = dataGridView1[2, i].Value.ToString();
+            comboBox1.Text = dataGridView1[3, i].Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ds = new DataSet();
+            adapter = new SqlDataAdapter("update tbl_accounts set username = '" + textBox1.Text + 
+                "', password = '" + textBox2.Text + 
+                "', privilege = '" + comboBox1.Text + 
+                "' where id = " + currentid, conn);
+            adapter.Fill(ds, "tbl_accounts");
+            GetRecords();
+            textBox1.Clear();
+            textBox2.Clear();
+            comboBox1.Text = "";
+        }
+    }
+}
